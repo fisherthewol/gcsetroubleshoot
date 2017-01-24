d3379a4a49fb165bbf6fab2fc4ac21e8415ef138
Fisher <george.kokinis@gmail.com>
2017-01-24 10:55:24 +0000

    Update solutions.py
    finished header; prepared for functions.

--- solutions.py
+++ solutions.py
@@ -1,2 +1,6 @@
 __author__ = "George Kokinis"
+__maintainer__ = "George Kokinis"
 __email__ = "george.kokinis@gmail.com, gkokinis@kes.sheffield.sch.uk"
+__status__ = "Development"
+__version__  =  0.1
+
