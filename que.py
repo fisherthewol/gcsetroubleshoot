8256f62e1e403e308dd449d9c7893b37c95b3244
Fisher <george.kokinis@gmail.com>
2017-01-26 13:03:57 +0000

    Update que.py
    PEP8 Check 2.

--- que.py
+++ que.py
@@ -3,9 +3,10 @@ __maintainer__ = "George Kokinis"
 __email__ = "george.kokinis@gmail.com, gkokinis@kes.sheffield.sch.uk"
 __status__ = "Development"
 __version__ = 0.20
-#This is a file for question functions; allows for //modularity//
+# This is a file for question functions; allows for //modularity//
 
-def warrantyInsure():  #Is the device insured or under warranty?
+
+def warrantyInsure():  # Is the device insured or under warranty?
     ans = input("Are you under Warranty or Insured with this device?")
     if ans == "yes" or "Yes" or "y" or "Y":
         return true
@@ -15,18 +16,21 @@ def warrantyInsure():  #Is the device insured or under warranty?
         print("I did not understand your input, please try again.")
         warrantyInsure()
 
-def problemQue():  #Does this device actually have problems?
+
+def problemQue():  # Does this device actually have problems?
     ans = input("Does your device have any problems?")
     if ans == "yes" or "Yes" or "y" or "Y":
-        print("Sorry, this program is primitive; \n we will now loop you back to the beginning and hope we can solve this.")
-        #CALL MAIN FUNCTION
+        print("Sorry, this program is primitive;"),
+        print("we will now loop you back to the beginning and hope we can solve this.")
+        # CALL MAIN FUNCTION
     elif ans == "no" or "No" or "n" or "N":
         print("Thanks for using this program; see you around!")
     else:
         print("I did not understand your input, please try again.")
         problemQue()
 
-def backUp(x):  #In main.py, x = deviceOS()
+        
+def backUp(x):  # In main.py, x = deviceOS()
     ans = input("Have you backed up your device recently?")
     if ans == "yes" or "Yes" or "y" or "Y":
         print("We suggest you fully reset your device;")
@@ -45,7 +49,8 @@ def backUp(x):  #In main.py, x = deviceOS()
         print("I did not understand your input, please try again.")
         backup()
 
-def infection():  #Is the device infected?
+
+def infection():  # Is the device infected?
     ans = input("Is your device infected?")
     if ans == "yes" or "Yes" or "y" or "Y":
         return true
@@ -54,3 +59,4 @@ def infection():  #Is the device infected?
     else:
         print("I did not understand your input, please try again.")
         infection()
+
