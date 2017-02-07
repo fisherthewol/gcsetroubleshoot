79406e167d9ecdfea493f0750f7c35de595c2a2d
fisherthewol <george.kokinis@gmail.com>
2017-02-07 19:49:07 +0000

    Update main.py
    status to testing from dev, version 0.5!

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 __author__ = "George Kokinis"
 __maintainer__ = "George Kokinis"
 __email__ = "george.kokinis@gmail.com, gkokinis@kes.sheffield.sch.uk"
-__status__ = "Development"
-__version__ = 0.49
+__status__ = "Tests"
+__version__ = 0.50
 # MainFile, includes question funcs - modularity is gone :(
 
 import sys
@@ -54,10 +54,48 @@ def warrantyInsure(x):  # Is the device insured or under warranty?
     sys.exit()
 
 
+def backUp(x):  # parse variable devicesOS
+    ans = str.lower(str(input("Have you backed up your device recently?")))
+    if ans == "yes" or "y":
+        print("We suggest you fully reset your device;"),
+        if x == "steve":
+            print("Plug your Device into your Computer, open itunes and reset it from there.")
+        elif x == "andy":
+            print("Go to settings, scroll and enter Backup and Settings, select Factory Data Reset and follow the prompts.")
+        elif x == "bill":
+            print("See https://support.microsoft.com/en-us/help/10666/windows-phone-reset-my-phone"),
+            print("Go to Settings, About Phone, select reset phone, and follow the prompts.")
+        else:
+            print("Error; somehow deviceOS wasn't called!")
+            deviceOS()
+        print("Thanks for using this program; see you around!")
+        sys.exit()
+    elif ans == "no" or "n":
+        print("We recommend you see a phone repair shop, especially a virus specialist."),
+        print("Thanks for using this program; see you around!")
+        sys.exit()
+    else:
+        print("I did not understand your input, please try again.")
+        backup()
+
+def problemQue():  # Does this device actually have problems?
+    ans = str.lower(str(input("Does your device have any problems?")))
+    if ans == "yes" or "y":
+        print("Sorry, this program is primitive;"),
+        print("we will now loop you back to the beginning and hope we can solve this.")
+        deviceOS()
+    elif ans == "no" or "n":
+        print("Thanks for using this program; see you around!")
+        sys.exit()
+    else:
+        print("I did not understand your input, please try again.")
+        problemQue()
+
+
 def infection():  # Is the device infected?
     ans = str.lower(str(input("Is your device infected?")))
     if ans == "yes" or "y":
-        backUp()
+        backUp(devicesOS)
     elif ans == "no" or "n":
         problemQue()
     else:
@@ -68,7 +106,7 @@ def infection():  # Is the device infected?
 def screenBroke():  # Is the screen broken?
     ans == str.lower(str(input("Is the screen/screen glass broken?")))
     if ans == "yes" or "y":
-        warrantyInsure()
+        warrantyInsure(devicesOS)
     elif ans == "no" or "n":
         infection()
     else:
@@ -123,37 +161,3 @@ def deviceOS():  # What OS does the device use/what manufacturer?
     else:
         print("I did not understand your input, please try again.")
         deviceOS()
-
-
-def problemQue():  # Does this device actually have problems?
-    ans = str.lower(str(input("Does your device have any problems?")))
-    if ans == "yes" or "y":
-        print("Sorry, this program is primitive;"),
-        print("we will now loop you back to the beginning and hope we can solve this.")
-        deviceOS()
-    elif ans == "no" or "n":
-        print("Thanks for using this program; see you around!")
-        sys.exit()
-    else:
-        print("I did not understand your input, please try again.")
-        problemQue()
-
-
-def backUp(x):  # parse variable of deviceOS
-    ans = str.lower(str(input("Have you backed up your device recently?")))
-    if ans == "yes" or "y":
-        print("We suggest you fully reset your device;"),
-        if x == "steve":
-            print("Plug your Device into your Computer, open itunes and reset it from there.")
-        elif x == "andy":
-            print("Go to settings, scroll and enter Backup and Settings, select Factory Data Reset and follow the prompts.")
-        elif x == "bill":
-            print("See https://support.microsoft.com/en-us/help/10666/windows-phone-reset-my-phone"),
-            print("Go to Settings, About Phone, select reset phone, and follow the prompts.")
-        print("Thanks for using this program; see you around!")
-    elif ans == "no" or "n":
-        print("We recommend you see a phone repair shop, especially a virus specialist."),
-        print("Thanks for using this program; see you around!")
-    else:
-        print("I did not understand your input, please try again.")
-        backup()
