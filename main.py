a973df8b50e299f52d898710ac7b7595c19a8280
Fisher <george.kokinis@gmail.com>
2017-03-22 08:37:24 +0000

    Update main.py
    Changed status because I forgot to.

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 __author__ = "George Kokinis"
 __maintainer__ = "George Kokinis"
 __email__ = "george.kokinis@gmail.com, gkokinis@kes.sheffield.sch.uk"
-__status__ = "Release"
+__status__ = "Development"
 __version__ = 1.12
 # MainFile, includes question funcs - modularity is gone :(
 # Imports
