b6f699730a0858bc025f8f99d8eb4afca1dd4bfd
fisherthewol <george.kokinis@gmail.com>
2017-03-30 18:11:20 +0100

    Update main.py
    Code is now inside function.

--- main.py
+++ main.py
@@ -1,17 +1,26 @@
+import sys
 __author__ = "George Kokinis"
 __maintainer__ = "George Kokinis"
 __email__ = "george.kokinis@gmail.com, gkokinis@kes.sheffield.sch.uk"
 __status__ = "Release"
-__version__ = 1.00
+__version__ = 1.02
 
 
-ans = str.lower(str(input("Which task do you want to launch?\n>")))
-if ans == "1" or ans == "one":
-    import task1
-    task1.deviceOS()
-elif ans == "2" or ans == "two":
-    import task2
-    task2.main()
-elif ans == "3" or ans == "three":
-    import task3
-    task3.main()
+def main():
+    ans = str.lower(str(input("Which task do you want to launch?\n>")))
+    if ans == "1" or ans == "one":
+        import task1
+        task1.deviceOS()
+    elif ans == "2" or ans == "two":
+        import task2
+        task2.main()
+    elif ans == "3" or ans == "three":
+        import task3
+        task3.main()
+    else:
+        print("Sorry, we did not understand your input.")
+        main()
+    sys.exit
+
+
+main()
